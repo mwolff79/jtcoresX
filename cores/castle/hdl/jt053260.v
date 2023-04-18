7e0771d55e406e3389ef69f1d3ea36c1ee17c4a6
flor <floryana.gonzalez@jotego.es>
2023-04-18 17:09:55 +0200

    wip k053260

--- cores/castle/hdl/jt053260.v
+++ cores/castle/hdl/jt053260.v
@@ -21,18 +21,51 @@ module jt053260 (
     input               rst,
     input               clk,
     input               cen,
+    input        [ 5:0] addr,
 
+    // Main CPU Address
+    input               ma0,
+    // Main CPU control
+    input               mrw,
+    input               mcs,
+    // Sub CPU control
+    input               rw,
     input               cs,
-    input        [20:0] addr,
-    input        [ 7:0] din,
+    // input YM2151
+    input               stb1,
+    input               aux1,
 
-    output              s0,
+    // Main CPU data bus
+    input        [ 7:0] mdb_din,
+    output       [ 7:0] mdb_dout,
+
+    // ROM Address and Data bus
+    output       [20:0] rom_addr,
+    output       [ 7:0] rom_dout,
+    input        [ 7:0] rom_din,
+
+    // YM2151
+    input        [ 7:0] db_din,
+    output       [ 7:0] db_dout,
+
+    // output bus YM-3012
+    output              so,
     output              sy,
     output              sh1,
-    output              sh2,
-    output       [ 7:0] dout
+    output              sh2
+
+
+    // slots disconnected
+    // input               st1,
+    // input               st2,
+    // input               aux2,
+    // output              rwp,
+    // output              tim2,
+    // output              ne,     // M6809 clock
+    // output              nq,     // M6809 clock
 
 );
+
 
 // 4 channels for register of 8 bit
 // reg    [7:0] ch0,
@@ -59,5 +92,56 @@ module jt053260 (
 //  D : -4
 //  E : -2
 //  F : -1
+
+wire    data;
+reg     pan,
+        loop,
+        start,
+        pitch,
+        key_on,
+        length,
+        volume;
+
+always @(posedge clk, posedge rst) begin
+    if(rst) begin
+        pan    <= 0;
+        loop   <= 0;
+        start  <= 0;
+        pitch  <= 0;
+        length <= 0;
+        volume <= 0;
+    end else begin
+
+    end
+end
+
+always @* begin
+    case (offset)
+        2'd0: pitch <= {pitch[], data[]};
+        2'd1: pitch <= {data[], pitch[]};
+        2'd2: length <= {length[], data[]};
+        2'd3: length <= {data[], length[]};
+        2'd4: start <= {start[], data[]};
+        2'd5: start <= {start[], data[], start[]};
+        2'd6: start <= {data[], start[]};
+        2'd7: volume <= data[];
+        default:;
+    endcase
+
+
+    case (offset)
+        2'h28: key_on[3:0] <= data;
+        2'h29: ;
+        2'h2A: loop[3:0] <= data;
+        2'h2B: ;
+        2'h2C: ;
+        2'h2D: ;
+        2'h2E: ;
+        2'h2F: ;
+        default:;
+    endcase
+
+end
+
 
 endmodule
